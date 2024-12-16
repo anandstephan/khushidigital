959a5584b5e5dddb54d818c813daacf8a6b186bf
Anand Stephan <anandstephan98@gmail.com>
2024-12-16 12:23:49 +0530

    feat:completed apk

---
+++ SplashScreen.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, View, Image, Dimensions} from 'react-native';
+
+const SplashScreen = ({onFinish}) => {
+  useEffect(() => {
+    // Simulate a delay (e.g., 3 seconds for splash)
+    const timer = setTimeout(() => {
+      onFinish(); // Notify the parent to proceed
+    }, 1000);
+
+    return () => clearTimeout(timer); // Cleanup
+  }, [onFinish]);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('./assets/logo.png')} // Update path as per your project
+        style={{width: '90%', height: '10%'}}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF', // Splash background color
+  },
+  logo: {
+    marginHorizontal: 20,
+    // width: 100, // Adjust width and height as per your logo
+    // height: 100,
+    // borderWidth: 2,
+  },
+});
+
+export default SplashScreen;
